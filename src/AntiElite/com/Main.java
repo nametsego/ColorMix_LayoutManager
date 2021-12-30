c6dd2795cc89851e5c7e7ecf64884e2d74854fd4
Nametsego <Nametsegothakadu@gmail.com>
2021-12-30 05:33:52 +0200

    Sub-Panel Commit

--- src/AntiElite/com/Main.java
+++ src/AntiElite/com/Main.java
@@ -25,7 +25,7 @@ public class Main {
         panel4.setBackground(Color.magenta);
         panel5.setBackground(Color.blue);
 
-        panel5.setLayout(new BoxLayout());
+        panel5.setLayout(new BorderLayout());
 
         panel1.setPreferredSize(new Dimension(100,100));
         panel2.setPreferredSize(new Dimension(100,100));
@@ -38,6 +38,35 @@ public class Main {
         frame.add(panel3,BorderLayout.EAST);
         frame.add(panel4,BorderLayout.SOUTH);
         frame.add(panel5,BorderLayout.CENTER);
+
+
+        //----- Sub Panels -----//
+
+
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel6.setBackground(Color.black);
+        panel7.setBackground(Color.darkGray);
+        panel8.setBackground(Color.gray);
+        panel9.setBackground(Color.lightGray);
+        panel10.setBackground(Color.white);
+
+        panel5.add(panel6,BorderLayout.NORTH);
+        panel5.add(panel7,BorderLayout.SOUTH);
+        panel5.add(panel8,BorderLayout.WEST);
+        panel5.add(panel9,BorderLayout.EAST);
+        panel5.add(panel10,BorderLayout.CENTER);
+
+        panel6.setPreferredSize(new Dimension(50,50));
+        panel7.setPreferredSize(new Dimension(50,50));
+        panel8.setPreferredSize(new Dimension(50,50));
+        panel9.setPreferredSize(new Dimension(50,50));
+        panel10.setPreferredSize(new Dimension(50,50));
+
 
     }
 }
